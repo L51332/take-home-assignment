f2aa6453d2b926d0750404f0d027f39bbf7a0061
L51332 <lavorgna1@gmail.com>
2022-11-14 14:37:18 -0800

    add initial search flow

--- cypress/e2e/github-flow.cy.js
+++ cypress/e2e/github-flow.cy.js
@@ -1,6 +1,36 @@
-describe('GitHub', ()=>{
+describe('GitHub workflows', ()=>{
+
+    // 'As a guest (not logged-in), when I search github for the term "create-react-app" from the landing page search input, I see:
+    //   - A count of matching results
+    //   - The facebook/create-create-app project as the first result
+    // - As a guest, when I click the "About" button in the landing page footer I am taken to the GitHub "About" page
+
+    beforeEach(()=>{
+        // cy.viewport(3456,2234);
+        cy.viewport(2560,1600);
+    });
+
+    it('should navigate to Github', ()=>{
+        cy.visit('https://www.github.com');
+        cy.url().then($url =>{
+            expect($url).to.eql('https://github.com/');
+        })
+    });
+
+    it('should show the search bar and click on it', ()=>{
+        cy.get('[data-test-selector="nav-search-input"]').should('exist').click();
+    });
+
+    it('should search for "create-react-app"', ()=>{
+        cy.get('[data-test-selector="nav-search-input"]').type('create-react-app{enter}');
+    });
+
+    it('should show a list of results', ()=>{
+        cy.get('ul.repo-list').should('exist');
+        cy.get('ul.repo-list').within(()=>{
+            cy.get('li').its('length').should('be.gte', 2);
+        });
+    });
 
 
-
-    
 });
